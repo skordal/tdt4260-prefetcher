11777dcd3c0b8e4292483206ec1dc94bb5293b47
Olav Emil Eiksund <olavemil@stud.ntnu.no>
2014-02-13 11:49:37 +0100

    updated prototype

--- rpt.py
+++ rpt.py
@@ -4,14 +4,13 @@ class entry:
 	def __init__(self, last_address):
 		self.last_address = la
 		self.state = 0
+		self.delta = 0
 	
 	def update(self, new_address):
 		new_delta = new_address - self.last_address
-		if state:
+		if delta:
 			if new_delta == self.delta:
 				prefetch(new_address, new_delta)
-		else:
-			self.state = 1
 		self.delta = new_delta
 		
 def on_miss(program_counter, miss_address):
